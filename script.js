90238d4d114df187905e85a7e954d49a260d5782
Csbw0319 <cwilkins0319@icloud.com>
2022-06-27 14:19:34 -0400

    added some javascript

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 console.log('connected')
 
+let currentImgIndex = 0;
+let previousImgIndex = 0;
 
-// create contact form
+
+const images = document.getElementsByClassName('images');
+const next = document.querySelector('.next');
+
+next.addEventListener('click', () => {
+    previousImgIndex = currentImgIndex++
+    images[previousImgIndex].style.display = 'none';
+images[currentImgIndex].style.display = 'block';
+if(currentImgIndex >= images.length) {
+    currentImgIndex = 0;
+   } 
+   
+})
+const prev = document.querySelector('.prev');
+
+prev.addEventListener('click', () => {
+previousImgIndex = currentImgIndex--
+images[previousImgIndex].style.display = 'none';
+images[currentImgIndex].style.display = 'block';
+})
+if(currentImgIndex < 0) {
+    currentImgIndex = images.length - 1;
+}
