0142f3aabec5b1e85ec272b362182af4d566ea87
Csbw0319 <cwilkins0319@icloud.com>
2022-06-27 14:47:18 -0400

    fixed carousel

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ const images = document.getElementsByClassName('images');
 const next = document.querySelector('.next');
 
 next.addEventListener('click', () => {
-    previousImgIndex = currentImgIndex++
-    images[previousImgIndex].style.display = 'none';
-images[currentImgIndex].style.display = 'block';
+previousImgIndex = currentImgIndex++
+images[previousImgIndex].style.display = 'none';
 if(currentImgIndex >= images.length) {
     currentImgIndex = 0;
    } 
+images[currentImgIndex].style.display = 'block';
    
 })
 const prev = document.querySelector('.prev');
@@ -21,8 +21,8 @@ const prev = document.querySelector('.prev');
 prev.addEventListener('click', () => {
 previousImgIndex = currentImgIndex--
 images[previousImgIndex].style.display = 'none';
-images[currentImgIndex].style.display = 'block';
-})
 if(currentImgIndex < 0) {
     currentImgIndex = images.length - 1;
 }
+images[currentImgIndex].style.display = 'block';
+})
